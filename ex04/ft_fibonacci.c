a3fb24ce7ae03b9b7c22cccfb2590869544bf14e
Celia FUSARO <cfusaro@e2r13p13.42.fr>
2017-11-14 06:40:15 +0100

    fibonacci

---
+++ ex04/ft_fibonacci.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_fibonacci.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cfusaro <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/09/09 18:08:42 by cfusaro           #+#    #+#             */
+/*   Updated: 2017/09/09 20:07:34 by cfusaro          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int		ft_fibonacci(int index)
+{
+	if (index < 0)
+		return (-1);
+	if (index == 0)
+		return (0);
+	else if (index == 1)
+		return (1);
+	else
+		return (ft_fibonacci(index - 1) + ft_fibonacci(index - 2));
+}
